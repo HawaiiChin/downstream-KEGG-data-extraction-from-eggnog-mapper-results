87d773c5969826a925c207f6c798164c8248e29f
Huayu Qin <46234762+HawaiiChin@users.noreply.github.com>
2026-01-05 19:41:51 +0800

    quantifying completeness of KEGG pathways
    in metagenome samples

---
+++ KEGG_pathway_completeness.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import sys
+
+ko_table=sys.argv[1]
+ko2path=sys.argv[2]
+outfile=sys.argv[3]
+
+ko = pd.read_csv(ko_table, sep="\t", index_col=0)
+ko = (ko > 0).astype(int)
+ko = ko.reset_index()
+ko = ko.rename(columns={ko.columns[0]:"KO"})
+
+ko["KO"]=ko["KO"].str.replace("^ko", "K", regex=True)
+
+map_df=pd.read_csv(ko2path, sep="\t", header = None, names=["KO","Pathway"])
+
+map_df["KO"]=map_df["KO"].str.replace("^ko:", "", regex=True)
+map_df["Pathway"]=map_df["Pathway"].str.replace("^path:","",regex=True)
+
+pathway_sizes= (map_df.groupby("Pathway")["KO"].nunique())
+
+merged = map_df.merge(ko,on="KO", how="inner"
+                      )
+
+present = (merged.groupby("Pathway")[ko.columns].sum()
+           )
+
+completeness=present.div(pathway_sizes, axis=0)
+completeness.to_csv(outfile, sep="\t")
